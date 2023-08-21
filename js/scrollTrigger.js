fa4c772a9253f5c3c988557479ccdaeedead324f
chtaynambk <islemchtayna@gmail.com>
2023-08-21 06:08:20 +0100

    Add files via upload

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -1,427 +1,474 @@
 gsap.registerPlugin(ScrollTrigger);
- 
-// gsap.to(".title", {
-//     scrollTrigger:{
-//         trigger:".title",
-//         toggleActions:"restart none none none",
-//         start:"top 30%",
-//         end:"top ",
 
-//         // scrub: true,
-//         markers: true
-//     },
-//     y: 50,
-//     duration: 3,
-//     // ease: "none"
+   function init(){
+  
 
-// });
+   }
+   window.addEventListener("load",function(event) {
+    init();
+   });
 
 
-// gsap.fromTo(".title" ,{
-//     y: -500,
-//     opacity: 0
-// },
-// {
-//     y:50,
-//     opacity: 1,
-//     duration: 2
+gsap.from(".title", {
+        duration: 1 ,
+        x: 50,
+        opacity: 0,
+       
+    });
 
-// }
-// );
-
-// tweenMax.from(".title " ,{
-//     y:500 ,
-//     // duartion: 5,
-//     // delay: 2
+  gsap.from(".social-media", {
+         duration: 1 ,
+        x: 50,
+        opacity: 0,
+       
+    });
 
 
-// });
-gsap.fromTo(".title", {
+   gsap.from(".box", {
+    duration: 1 ,
+   y: 50,
+   autoAlpha:0,
+
+   
+   scrollTrigger:{
+    trigger:".box",
+    toggleActions: "restart none none reverse",
+    start:"top center",
+    end:"top 50% ",
+    // markers:true,
+    // scrub: 1
+
+   }
+  
+});
+
+
+gsap.from(".logo-ipsum", {
+    duration: 0.5,
     x: -50,
-    opacity: 0 
-},
-{
-    scrollTrigger:{
-        trigger:".title",
-        toggleActions:"restart none none none",
-        start:"center 50%",
-        end:"center 10% ",
-        // markers:true
-    },
-    x: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1 
-});
-
-gsap.fromTo(".social-media", {
-    x: 50,
-    opacity: 0 
-},
-{
-    scrollTrigger:{
-        trigger:".social-media",
-        toggleActions:"restart none none none",
-        start:"center 50%",
-        end:"center 10% ",
-        // markers:true
-    },
-    x: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1 
-});
-
-gsap.fromTo(".box", {
-    x: 50,
-    opacity: 0
-},
-{
-    scrollTrigger:{
-        trigger:".box",
-        toggleActions:"restart none none none",
-        start:"top center",
-        end:"top 80% ",
-        // markers:true,
-        scrub: 1
-    },
-    x: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
-});
-gsap.fromTo(".logo-ipsum", {
-    x: -50,
-    opacity: 0
-},
-{
+    opacity: 0,
+    
     scrollTrigger:{
         trigger:".logo-ipsum",
-        toggleActions:"restart none none none",
-        start:"top 60%",
-        end:"top 80% ",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
-        scrub: 1
-    },
-    x: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+        // scrub: 1
+    }
 });
 
-gsap.fromTo(".content", {
+
+// animation left to right for each
+
+// const textBox = gsap.utils.toArray(".content");
+
+// textBox.forEach(content => {
+//   gsap.from(content, { 
+//     x: 50,
+//     opacity: 0,
+//     duration:1,
+//       scrollTrigger: {
+//       trigger: content,
+//       start:"top center",
+//       end:"top 30%",
+//       toggleActions: 'restart none none none',
+//       markers:true,
+//       scrub: 1,
+//       delay:1
+
+//     }
+//   })
+// });
+
+
+
+const textBox = gsap.utils.toArray('.content');
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".text-box",
+    toggleActions:"restart none none reverse",
+    start:"top 60%",
+    end:"top 50%",
+
+    // scrub: 1,
+  }
+});
+
+textBox.forEach((content) => {
+  tl.from(content, { y: 50, duration: 0.2, opacity: 0 })
+});
+
+
+
+
+
+
+
+const pragh = gsap.utils.toArray("p");
+
+pragh.forEach(p => {
+  gsap.from(p, { 
+    y: 50,
+    opacity: 0,
+    duration:1,
+      scrollTrigger: {
+      trigger: p,
+      start:"top 80%",
+      end:"top 50%",
+      toggleActions: 'restart none none reverse',
+    //   markers:true,
+    //   scrub: 1,
+    //   delay:1
+
+    }
+  })
+});
+
+
+
+
+
+// var tl = gsap.timeline({repeat: 2, repeatDelay: 1});
+
+// tl.from(".content", {
+//     y: 50,
+//     opacity: 0,
+
+//     scrollTrigger:{
+//         trigger:".content",
+//         toggleActions:"restart none none none",
+//         start:"top 60%",
+//         end:"top 40% ",
+//         markers:true,
+//         scrub: 1
+//     },
+
+// });
+
+
+gsap.from(".img-cont", {
+    duration: 0.5,
     x: -50,
-    opacity: 0
-},
-{
-    scrollTrigger:{
-        trigger:".content",
-        toggleActions:"restart none none none",
-        start:"top 60%",
-        end:"top 40% ",
-        // markers:true,
-        scrub: 1
-    },
-    x: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
-});
-
-gsap.fromTo(".img-cont", {
-    x: 50,
-    opacity: 0
-},
-{
+   autoAlpha:0,
+    
     scrollTrigger:{
         trigger:".img-cont",
-        toggleActions:"restart none none none",
-        start:"top 60%",
-        end:"top 40% ",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
-        scrub: 1
-    },
-    x: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+        // scrub: 1
+    }
 });
 
 
-gsap.fromTo(".inner-text", {
-    x: -50,
-    opacity: 0
-},
-{
+
+gsap.from(".inner-text-cont", {
+    duration: 0.5,
+    x: 50,
+   autoAlpha:0,
+    
     scrollTrigger:{
-        trigger:".inner-text",
-        toggleActions:"restart none none none",
-        start:"top 60%",
-        end:"top 40% ",
+        trigger:".inner-text-cont",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
-        scrub: 1
-    },
-    x: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+        // scrub: 1
+    }
 });
 
 
-gsap.fromTo(".business-text", {
+gsap.from(".business-text", {
+    duration: 0.5,
     x: -50,
-    opacity: 0
-},
-{
+   autoAlpha:0,
+    
     scrollTrigger:{
         trigger:".business-text",
-        toggleActions:"restart none none none",
-        start:"top 60%",
-        end:"top 40% ",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
-        scrub: 1
-    },
-    x: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+        // scrub: 1
+    }
 });
 
 
-gsap.fromTo(".video", {
-    x: 50,
-    opacity: 0
-},
-{
+gsap.from(".video", {
+    duration: 0.5,
+    y: 50,
+   autoAlpha:0,
+    
     scrollTrigger:{
         trigger:".video",
-        toggleActions:"restart none none none",
-        start:"top 60%",
-        end:"top 40% ",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
-        scrub: 1
-    },
-    x: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+        // scrub: 1
+    }
 });
 
-
-gsap.fromTo(".skils-bg", {
+gsap.from(".skils-bg", {
+    duration: 0.5,
     y: 100,
-    opacity: 0
-},
-{
+   autoAlpha:0,
+    
     scrollTrigger:{
         trigger:".skils-bg",
         toggleActions:"restart none none none",
-        start:"top 80%",
-        end:"top 40% ",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
-        scrub: 1
-    },
-    y: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+        // scrub: 1
+    }
 });
 
 
-gsap.fromTo(".skil-bar1", {
-    width: 0,
-    opacity: 0
-},
-{
+gsap.from(".skil-bar1", {
+    duration: 0.5,
+    width:0,
+   autoAlpha:0,
+    
     scrollTrigger:{
         trigger:".skil-bar1",
-        // toggleActions:"restart none none none",
-        start:"top 60%",
-        end:"top 40% ",
+        toggleActions:"restart none none none",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
         // scrub: 1
-    },
-    width: "95%", 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+    }
 });
 
-gsap.fromTo(".skil-bar2", {
-    width: 0,
-    opacity: 0
-},
-{
+
+gsap.from(".skil-bar2", {
+    duration: 0.5,
+    width:0,
+   autoAlpha:0,
+    
     scrollTrigger:{
         trigger:".skil-bar2",
-        // toggleActions:"restart none none none",
-        start:"top 60%",
-        end:"top 40% ",
+        toggleActions:"restart none none none",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
         // scrub: 1
-    },
-    width: "95%", 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
-});
-
-gsap.fromTo(".skil-bar3", {
-    width: 0,
-    opacity: 0
-},
-{
+    }
+});gsap.from(".skil-bar3", {
+    duration: 0.5,
+    width:0,
+   autoAlpha:0,
+    
     scrollTrigger:{
         trigger:".skil-bar3",
-        // toggleActions:"restart none none none",
-        start:"top 60%",
-        end:"top 40% ",
+        toggleActions:"restart none none none",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
         // scrub: 1
-    },
-    width: "95%", 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
-});
-
-gsap.fromTo(".skil-bar4", {
-    width: 0,
-    opacity: 0
-},
-{
+    }
+});gsap.from(".skil-bar4", {
+    duration: 0.5,
+    width:0,
+   autoAlpha:0,
+    
     scrollTrigger:{
         trigger:".skil-bar4",
-        // toggleActions:"restart none none none",
-        start:"top 60%",
-        end:"top 40% ",
+        toggleActions:"restart none none none",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
         // scrub: 1
-    },
-    width: "95%", 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+    }
 });
 
-gsap.fromTo(".violet-mode", {
+gsap.from(".violet-mode", {
+    duration: 0.5,
     x: -50,
-    opacity: 0
-},
-{
+   autoAlpha:0,
+    
     scrollTrigger:{
-        trigger:".card-content",
-        toggleActions:"restart none none none",
-        start:"top 60%",
-        end:"top 10% ",
+        trigger:".violet-mode",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
-        scrub: 1
-    },
-    x: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+        // scrub: 1
+    }
 });
 
 
-gsap.fromTo(".card-content", {
+
+gsap.from(".white-mode", {
+    duration: 0.5,
     x: 50,
-    opacity: 0
-},
-{
+   autoAlpha:0,
+    
     scrollTrigger:{
-        trigger:".card-content",
-        toggleActions:"restart none none none",
-        start:"top 80%",
-        end:"top 40% ",
+        trigger:".white-mode",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
-        scrub: 1
-    },
-    x: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+        // scrub: 1
+    }
 });
 
-gsap.fromTo(".imag-inr", {
+gsap.from(".imag-inr", {
+    duration: 0.5,
     x: -50,
-    opacity: 0
-},
-{
+   autoAlpha:0,
+    
     scrollTrigger:{
         trigger:".imag-inr",
-        toggleActions:"restart none none none",
-        start:"top 80%",
-        end:"top 10% ",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
-        scrub: 1
-    },
-    x: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+        // scrub: 1
+    }
 });
 
-gsap.fromTo(".text-right", {
+
+gsap.from(".text-right", {
+    duration: 0.5,
     x: 50,
-    opacity: 0
-},
-{
+   autoAlpha:0,
+    
     scrollTrigger:{
         trigger:".text-right",
-        toggleActions:"restart none none none",
-        start:"top 80%",
-        end:"top 10% ",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
-        scrub: 1
-    },
-    x: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+        // scrub: 1
+    }
 });
 
-gsap.fromTo(".openday", {
+gsap.from(".openday", {
+    duration: 0.5,
     y: 50,
-    opacity: 0
-},
-{
+   autoAlpha:0,
+    
     scrollTrigger:{
         trigger:".openday",
-        toggleActions:"restart none none none",
-        start:"top 80%",
-        end:"top 40% ",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
-        scrub: 1
-    },
-    y: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+        // scrub: 1
+    }
 });
 
 
-gsap.fromTo(".all-cont", {
+
+gsap.from(".all-cont", {
+    duration: 0.5,
     y: 50,
-    opacity: 0
-},
-{
+   autoAlpha:0,
+    
     scrollTrigger:{
         trigger:".all-cont",
-        toggleActions:"restart none none none",
-        start:"top 80%",
-        end:"top 40% ",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
         // markers:true,
-        scrub: 1
-    },
-    y: 0, 
-    duration: 1 ,
-    ease:"in",
-    opacity:1
+        // scrub: 1
+    }
 });
 
 
+const anime = gsap.utils.toArray('.prtfo');
+
+let tm = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".prt",
+    toggleActions:"restart none none reverse",
+    start:"top 60%",
+    end:"top 50%",
+    // markers:true,
+ 
+    // scrub: 1,
+  }
+});
+
+anime.forEach((prtfo) => {
+  tm.from(prtfo, { y: 50, duration: 0.2, opacity: 0 })
+});
+
+// animation left to right for each
+
+// const animation = gsap.utils.toArray(".prtfo");
+
+// animation.forEach(prtfo => {
+//   gsap.from(prtfo, { 
+//     x: 50,
+//     opacity: 0,
+//     duration:1,
+//       scrollTrigger: {
+//       trigger: prtfo,
+//       start:"top center",
+//       end:"top 30%",
+//       toggleActions: 'restart none none none',
+//       markers:true,
+//       scrub: 1,
+//       delay:1
+
+//     }
+//   })
+// });
+
+gsap.from(".inf", {
+    duration: 0.5,
+    y: 50,
+   autoAlpha:0,
+    
+    scrollTrigger:{
+        trigger:".inf",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
+        // markers:true,
+        // scrub: 1
+    }
+});
 
 
+gsap.from(".contact-box", {
+    duration: 0.5,
+    y: 50,
+   autoAlpha:0,
+    
+    scrollTrigger:{
+        trigger:".grid",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
+        // markers:true,
+        // scrub: 1
+    }
+});
 
+gsap.from(".inf-flex", {
+    duration: 0.5,
+    y: 50,
+   autoAlpha:0,
+    
+    scrollTrigger:{
+        trigger:".inf-flex",
+        toggleActions:"restart none none reverse",
+        start:"top 70%",
+        end:"top 50% ",
+        // markers:true,
+        // scrub: 1
+    }
+});
+
+
+   
+   
+  
 
